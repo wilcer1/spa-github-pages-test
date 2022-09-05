a3424ae46908ccdeda21d38137f2de59342e069f
wilcer1 <wille.cervin@live.se>
2022-09-05 11:34:39 +0200

    testing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,96 @@
-import React from 'react';
-import InteractiveLink from './InteractiveLink';
-import { Code, P } from './UI';
+import React, { useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect } from 'react';
+import '../style/Home.css';
 
-const pageLinkContainerStyle = {
-  display: 'block',
-  margin: '1vh 0',
-};
+function Home() {
+  const boxRef = useRef();
 
-const RepoReadmeLink = () => (
-  <InteractiveLink href="https://github.com/rafgraph/spa-github-pages#readme">
-    repo readme
-  </InteractiveLink>
-);
+  useEffect(() => {
+    // gsap.fromTo(
+    //   boxRef.current,
+    //   { y: 100, autoAlpha: 0 },
+    //   {
+    //     duration: 2.5,
+    //     y: 0,
+    //     autoAlpha: 1,
+    //     ease: "back",
+    //     overwrite: "auto"
+    //   }
+    // );
 
-export default function Home() {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.utils.toArray('.revealUp').forEach(function (elem) {
+      ScrollTrigger.create({
+        trigger: elem,
+        start: 'top 70%',
+        end: 'bottom 20%',
+        onEnter: function () {
+          gsap.fromTo(
+            elem,
+            { y: 100, autoAlpha: 0 },
+            {
+              duration: 1.25,
+              y: 0,
+              autoAlpha: 1,
+              ease: 'back',
+              overwrite: 'auto',
+            },
+          );
+        },
+        onLeave: function () {
+          gsap.fromTo(
+            elem,
+            { autoAlpha: 1 },
+            { autoAlpha: 0, overwrite: 'auto' },
+          );
+        },
+        onEnterBack: function () {
+          gsap.fromTo(
+            elem,
+            { y: -100, autoAlpha: 0 },
+            {
+              duration: 1.25,
+              y: 0,
+              autoAlpha: 1,
+              ease: 'back',
+              overwrite: 'auto',
+            },
+          );
+        },
+        onLeaveBack: function () {
+          gsap.fromTo(
+            elem,
+            { autoAlpha: 1 },
+            { autoAlpha: 0, overwrite: 'auto' },
+          );
+        },
+      });
+    });
+  });
+
   return (
-    <div>
-      <P>
-        This is an example single page app built with React and
-        React&nbsp;Router using <Code>BrowserRouter</Code>. Navigate with the
-        links below and refresh the page or copy/paste the url to test out the
-        redirect functionality deployed to overcome GitHub&nbsp;Pages
-        incompatibility with single page apps (like this one).
-      </P>
-      <P>
-        Please see the <RepoReadmeLink /> for instructions on how to use this
-        boilerplate to deploy your own single page app using GitHub Pages.
-      </P>
-      <P>
-        <span style={pageLinkContainerStyle}>
-          <InteractiveLink to="/example">Example page</InteractiveLink>
-        </span>
-        <span style={pageLinkContainerStyle}>
-          <InteractiveLink to="/example/two-deep?field1=foo&field2=bar#boom!">
-            Example two deep with query and hash
-          </InteractiveLink>
-        </span>
-      </P>
-      <P>
-        <InteractiveLink to="/sitemap-link-generator">
-          Sitemap Link Generator
-        </InteractiveLink>
-      </P>
-    </div>
+    <>
+      <div className="box" ref={boxRef}>
+        <h1 className="home">
+          <span>
+            There are no limits to what you can accomplish, except the limits
+            you place on your own thinking.
+          </span>
+          <a href="/boka">
+            <button className="HomeBooking">Boka</button>
+          </a>
+        </h1>
+      </div>
+      <div className="placeholder">
+        <div className="revealUp">Testing</div>
+      </div>
+      <div className="placeholder2">
+        <div className="revealUp">Glazers Out</div>
+      </div>
+    </>
   );
 }
+
+export default Home;
