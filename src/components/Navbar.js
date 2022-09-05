f0e685fbc77da067ba83171f22c78248c2dd7212
wilcer1 <wille.cervin@live.se>
2022-09-05 11:59:52 +0200

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useEffect } from 'react';
+import '../style/Navbar.css';
+import instagram from '../assets/Instagram-Icon.png';
+
+function Navbar() {
+  useEffect(() => {}, []);
+
+  return (
+    <>
+      <header className="menu">
+        <div className="menu-wrap">
+          <input type="checkbox" id="checkbox"></input>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">
+                  <h1>Företag namn här</h1>
+                </a>
+              </li>
+              <li className="endpoints">
+                <a href="/Boka">Boka Tid</a>
+              </li>
+              <li className="endpoints">
+                <a href="/Om">Om</a>
+              </li>
+              <li className="socialmedia">
+                {' '}
+                <a href="https://www.instagram.com/holistisklakning/">
+                  <img
+                    src={instagram}
+                    alt="instagram"
+                    width={30}
+                    height={30}
+                  ></img>
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <label htmlFor="checkbox">
+            <i className="fa fa-bars menu-icon"></i>
+          </label>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,14 @@ module.exports = {
   },
 
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-   {test: /\.css$/,
-    use: ["style-loader", "css-loader"]}
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+   {test: /\.css$/, use: ["style-loader", "css-loader"],},
+    {test: /\.(png|jpe?g|gif)$/i,  use: [
+      {
+        loader: 'file-loader',
+      },
+    ],}
     
   ],
   },
