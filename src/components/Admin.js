18ff831d0d5ee2e08607bbee9556324b12ce96e4
wilcer1 <wille.cervin@live.se>
2022-09-06 10:27:22 +0200

    delete readme

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,55 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AdminCalendar from './AdminCalendar';
 
-function Login() {
-  const [user, setUser] = useState('');
-  const [pass, setPass] = useState('');
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
+function Admin() {
   return (
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p>Username</p>
-          <input
-            type="text"
-            id="user"
-            name="user"
-            onChange={(event) => setUser(event.target.value)}
-            value={user}
-          />
-        </label>
-        <label>
-          <p>Password</p>
-          <input
-            type="password"
-            id="user"
-            name="user"
-            onChange={(event) => setPass(event.target.value)}
-            value={pass}
-          />
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+    <div className="AdminCalendar">
+      <AdminCalendar />
     </div>
   );
-}
-
-function Admin() {
-  const [auth, setauth] = useState(false);
-
-  if (!auth) {
-    return <Login />;
-  } else
-    return (
-      <div className="AdminCalendar">
-        <AdminCalendar />
-      </div>
-    );
 }
 
 export default Admin;
