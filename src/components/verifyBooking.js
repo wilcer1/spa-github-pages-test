e03d309e928c35ebbbd129d27a85629e239540eb
wilcer1 <wille.cervin@live.se>
2022-09-09 18:26:32 +0200

    google button working :DDDD

--- src/components/verifyBooking.js
+++ src/components/verifyBooking.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import GoogleLogin from 'react-google-login';
-
-const responseGoogle = (response) => {
-  console.log(response);
-};
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 
 function VerifyBooking() {
   return (
-    <div className="VerifyBooking">
-      <h1>Testing</h1>
-      <GoogleLogin
-        clientId="310046150503-9oopaeieiv9p6l5t7qmiitev0jugfuvu.apps.googleusercontent.com"
-        render={(renderProps) => (
-          <button onClick={renderProps.onClick} disabled={renderProps.disabled}>
-            Verify booking with Google
-          </button>
-        )}
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
-    </div>
+    <GoogleOAuthProvider clientId="916587672516-pib163h8ridhcoknethuiq0l3d3ahbc6.apps.googleusercontent.com">
+      ...;
+      <div className="VerifyBooking">
+        <h1>Testing</h1>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse, credentialResponse);
+            localStorage.setItem('Token', 'hey');
+          }}
+          onError={(error) => {
+            console.log('Login Failed', error);
+          }}
+        />
+        ;
+      </div>
+    </GoogleOAuthProvider>
   );
 }
 
