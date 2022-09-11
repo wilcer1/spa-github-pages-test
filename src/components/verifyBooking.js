20177eb584824facdf4776e05302f905e67dad4f
wilcer1 <wille.cervin@live.se>
2022-09-11 13:41:58 +0200

    testing

--- src/components/verifyBooking.js
+++ src/components/verifyBooking.js
@@ -5,6 +5,7 @@ import '../style/VerifyBooking.css';
 
 function VerifyBooking() {
   const responseFacebook = (response) => {
+    console.log(response);
     if (response) {
       localStorage.setItem('F-Token', response);
     }
