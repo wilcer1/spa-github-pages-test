1b40311beda68f917ac5c24b0cf1229c7d0d1c01
wilcer1 <wille.cervin@live.se>
2022-09-13 17:35:08 +0200

    fuck this

---
+++ index.ts
@@ -0,0 +1,9 @@
+export { default as GoogleOAuthProvider } from './src/components/GoogleLogin/GoogleOAuthProvider';
+export { default as GoogleLogin } from './src/components/GoogleLogin/GoogleLogin';
+export type { GoogleLoginProps } from './src/components/GoogleLogin/GoogleLogin';
+export { default as googleLogout } from './src/components/GoogleLogin/googleLogout';
+export { default as useGoogleLogin } from './src/components/GoogleLogin/hooks/useGoogleLogin';
+export { default as useGoogleOneTapLogin } from './src/components/GoogleLogin/hooks/useGoogleOneTapLogin';
+export { default as hasGrantedAllScopesGoogle } from './src/components/GoogleLogin/hasGrantedAllScopesGoogle';
+export { default as hasGrantedAnyScopeGoogle } from './src/components/GoogleLogin/hasGrantedAnyScopeGoogle';
+export * from './src/components/GoogleLogin/types';
